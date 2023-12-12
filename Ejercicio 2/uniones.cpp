792abbc60d27af286384102245fc506936dca152
maritoo17 <msancgim@myuax.com>
2023-12-12 12:24:28 +0100

    Última parte del ejercicio en la que añado la función main y le doy unos valores a las distintas incógnitas que tenemos.

--- Ejercicio 2/uniones.cpp
+++ Ejercicio 2/uniones.cpp
@@ -8,3 +8,18 @@ union Datos{
     float flotante;
     char cadena[30];
 };
+
+int main() {
+    union Datos datos_union;
+
+    datos_union.entero = 10;
+    printf("Entero: %d\n", datos_union.entero);
+
+    datos_union.flotante = 3.1416;
+    printf("Flotante: %.2f\n", datos_union.flotante);
+
+    strcpy(datos_union.cadena, "Hola mundo");
+    printf("Cadena: %s\n", datos_union.cadena);
+
+    return 0;
+}
