1d90b94c4ee8c034bd5a85258bc62f3d5e08df6b
maritoo17 <msancgim@myuax.com>
2023-12-12 12:40:35 +0100

    Cuarta parte en la que añado la copia del estudiante y hago que se imprima tanto el original como la copia.

--- Ejercicio 3/copias.cpp
+++ Ejercicio 3/copias.cpp
@@ -15,3 +15,17 @@ void copiarEstudiante(struct Estudiante *destino, const struct Estudiante *orige
     destino->edad = origen->edad;
     destino->promedio = origen->promedio;
 }
+
+int main() {
+    struct Estudiante estudianteOriginal = {"Juan", 20, 9.5, NULL};
+
+    struct Estudiante studianteCopia;
+
+    copiarEstudiante(&studianteCopia, &estudianteOriginal);
+
+
+    printf ("Estudiante original: %s, %d años, promedio %.2f\n", estudianteOriginal.nombre, estudianteOriginal.edad, estudianteOriginal.promedio);
+    printf ("Estudiante copia: %s, %d años, promedio %.2f\n", studianteCopia.nombre, studianteCopia.edad, studianteCopia.promedio);
+
+    return 0;
+}
