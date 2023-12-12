e4731bada9ffdbb4e93f36294347ae0b634c66a5
maritoo17 <msancgim@myuax.com>
2023-12-12 13:15:42 +0100

    Última parte del ejercicio en la que añado la función main que será la encargada de que la función funcione con normalidad.

--- Ejercicio 5/funciones.cpp
+++ Ejercicio 5/funciones.cpp
@@ -13,3 +13,11 @@ void imprimirEstudiante(struct Estudiante estudiante) {
     printf("Edad: %d\n", estudiante.edad);
     printf("Promedio: %.2f\n", estudiante.promedio);
 }
+
+int main() {
+    struct Estudiante estudiante = {"Juan", 20, 9.5};
+
+    imprimirEstudiante(estudiante);
+
+    return 0;
+}
