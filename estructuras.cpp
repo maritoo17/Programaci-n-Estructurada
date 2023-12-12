8422072de9e67374193b53270ef4d1fe73a92cdb
maritoo17 <msancgim@myuax.com>
2023-12-12 11:51:35 +0100

    Segunda parte en la que le asigno unos valores al nombre, a la edad y al promedio.

--- estructuras.cpp
+++ estructuras.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 struct Estudiante {
-    string nombre;
+    char nombre[30];
     int edad;
     float promedio;
 };
+
+int main(){
+    struct Estudiante estudiante1;
+
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
