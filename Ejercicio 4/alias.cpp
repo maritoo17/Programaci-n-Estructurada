2640661e4546d5b9dba89aba2f46afc9cee60fe4
maritoo17 <msancgim@myuax.com>
2023-12-12 12:59:07 +0100

    Cuarta parte en la que añado el alias que nos piden y acabo el ejercicio.

--- Ejercicio 4/alias.cpp
+++ Ejercicio 4/alias.cpp
@@ -10,3 +10,13 @@ struct Estudiante {
 };
 
 typedef struct Estudiante EstudianteAlias;
+
+int main() {
+    EstudianteAlias estudiante = {"Juan", 20, 9.5};
+
+    printf("Nombre: %s\n", estudiante.nombre);
+    printf("Edad: %d\n", estudiante.edad);
+    printf("Promedio: %.2f\n", estudiante.promedio);
+
+    return 0;
+}
